d86e90adaa3d0da831a7e115474bbe42183cbe71
LittleGreen-z <zhao2946@sina.com>
2019-05-02 15:15:40 +0800

    M面包屑

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="dd">
     <el-container class="container">
       <el-header>
         <el-row>
@@ -10,7 +10,7 @@
             <h2>电商后台管理系统</h2>
           </el-col>
           <el-col :span="1" class="logout">
-            <a href="#" class="logout">退出</a>
+            <a href="#" class="logout" @click.prevent="handleLoginout()">退出</a>
           </el-col>
         </el-row>
       </el-header>
@@ -30,7 +30,7 @@
                 <i class="el-icon-location"></i>
                 <span>用户管理</span>
               </template>
-              <el-menu-item index="1-1">
+              <el-menu-item index="users">
                 <i class="el-icon-s-grid"></i>
                 <span>用户列表</span>
                 </el-menu-item>
@@ -93,7 +93,9 @@
             </el-submenu>
           </el-menu>
         </el-aside>
-        <el-main class="main">Main</el-main>
+        <el-main class="main">
+          <router-view></router-view>
+        </el-main>
       </el-container>
     </el-container>
   </div>
@@ -106,20 +108,34 @@ export default {
       this.$router.push({
         name: 'login'
       })
+      this.$message.warning('请先登录')
     }
-    this.$message.warning('请先登录')
+  },
+  methods: {
+    handleLoginout () {
+      localStorage.clear()
+      this.$router.push({
+        name: 'login'
+      })
+      this.$message.warning('退出成功')
+    }
   }
 }
 </script>
 
 <style>
+.dd {
+  height: 100%;
+}
 .container {
   background-color: #b3c0d1;
+  height: 100%;
 }
 .middle {
   line-height: 60px;
   text-align: center;
   color: #ffffff;
+  height: 100%
 }
 .logout {
   line-height: 60px;
---
+++ src/components/users.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class=td>
+    <el-card class="box">
+      <!-- 面包屑 -->
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+        <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+      </el-breadcrumb>
+      <el-row class="seartBox">
+        <el-col>
+          <el-input class="searchInput" placeholder="请输入内容" v-model="query">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+          <el-button type="success">添加用户</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      query: ''
+    }
+  }
+}
+</script>
+
+<style>
+.td {
+  height: 100%;
+}
+.box {
+  height: 100%
+}
+.seartBox {
+  margin-top: 20px
+}
+.searchInput {
+  width: 350px
+}
+</style>
