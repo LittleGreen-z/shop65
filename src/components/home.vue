c818a62a90c252d9bec5b4f354b08733b86145a7
LittleGreen-z <zhao2946@sina.com>
2019-05-02 14:21:39 +0800

    权限验证

--- src/components/home.vue
+++ src/components/home.vue
@@ -15,7 +15,84 @@
         </el-row>
       </el-header>
       <el-container>
-        <el-aside width="200px" class="aside">Aside</el-aside>
+        <el-aside width="200px" class="aside">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            :unique-opened="true"
+            :router="true"
+            @open="handleOpen"
+            @close="handleClose"
+          >
+          <!-- 导航一 -->
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>用户管理</span>
+              </template>
+              <el-menu-item index="1-1">
+                <i class="el-icon-s-grid"></i>
+                <span>用户列表</span>
+                </el-menu-item>
+            </el-submenu>
+            <!-- 导航二 -->
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>权限管理</span>
+              </template>
+              <el-menu-item index="1-1">
+                <i class="el-icon-s-grid"></i>
+                <span>角色列表</span>
+                </el-menu-item>
+                <el-menu-item index="1-1">
+                <i class="el-icon-s-grid"></i>
+                <span>权限列表</span>
+                </el-menu-item>
+            </el-submenu>
+            <!-- 导航三 -->
+            <el-submenu index="3">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>商品管理</span>
+              </template>
+              <el-menu-item index="1-1">
+                <i class="el-icon-s-grid"></i>
+                <span>商品列表</span>
+                </el-menu-item>
+                <el-menu-item index="1-1">
+                <i class="el-icon-s-grid"></i>
+                <span>分类参数</span>
+                </el-menu-item>
+                <el-menu-item index="1-1">
+                <i class="el-icon-s-grid"></i>
+                <span>商品分类</span>
+                </el-menu-item>
+            </el-submenu>
+            <!-- 导航四 -->
+            <el-submenu index="4">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>订单管理</span>
+              </template>
+              <el-menu-item index="1-1">
+                <i class="el-icon-s-grid"></i>
+                <span>订单列表</span>
+                </el-menu-item>
+            </el-submenu>
+            <!-- 导航五-->
+            <el-submenu index="5">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>数据统计</span>
+              </template>
+              <el-menu-item index="1-1">
+                <i class="el-icon-s-grid"></i>
+                <span>数据报表</span>
+                </el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
         <el-main class="main">Main</el-main>
       </el-container>
     </el-container>
@@ -24,21 +101,28 @@
 
 <script>
 export default {
-
+  beforeMount () {
+    if (!localStorage.getItem('token')) {
+      this.$router.push({
+        name: 'login'
+      })
+    }
+    this.$message.warning('请先登录')
+  }
 }
 </script>
 
 <style>
 .container {
-  background-color: #b3c0d1
+  background-color: #b3c0d1;
 }
 .middle {
   line-height: 60px;
   text-align: center;
-  color: #ffffff
+  color: #ffffff;
 }
 .logout {
   line-height: 60px;
-  text-decoration: none
+  text-decoration: none;
 }
 </style>
